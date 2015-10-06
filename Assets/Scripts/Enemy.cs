fb15769ac86b9bd9c81a4d3850591adaebfb91b7
MadsChris92 <madschris888@gmail.com>
2015-10-06 18:21:12 +0200

    fasdf

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 	public GameObject enemy;
     public float health = 100;
+    public FollowPath path;
 	GameObject bullet;
     public EnemyType enemyType;
 
@@ -14,11 +15,17 @@ public class Enemy : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
- 
+        path = path.GetComponent<FollowPath>();
     bullet = GameObject.FindGameObjectWithTag ("Bullet");
         if (enemyType == EnemyType.basic)
         {
             health = health * 2;
+            path.Speed = 1;
+        }
+        if(enemyType == EnemyType.moderat)
+        {
+            health = health * 3;
+            path.Speed = 10;
         }
     }
 	
